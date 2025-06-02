22b26ca11198f8268e5577308562a410019cad00
MuratKB <kentbahcesiofficial@gmail.com>
2025-06-02 14:52:18 +0300

    Updated shop.ts

---
+++ src/types/shop.ts
@@ -0,0 +1,28 @@
+export interface ProductSize {
+  size: string;
+  unit: string;
+  price: number;
+  inStock: boolean;
+}
+
+export interface ShopProduct {
+  id: string;
+  name: string;
+  description: string;
+  image: string;
+  category: string;
+  sizes: ProductSize[];
+  minOrder?: number;
+}
+
+export interface CheckoutFormData {
+  name: string;
+  email: string;
+  company: string;
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+  postalCode: string;
+  phone: string;
+}
